1100116f8c43111407f2ea9f43f5e7d8c9244e00
JoeShirley <joe.shirley@keyesinfotech.com>
2019-06-03 15:15:19 -0400

    added relfrequecny.py

---
+++ relfrequency.py
@@ -0,0 +1,15 @@
+items = []
+try:
+    while 1:
+        new = raw_input("Enter a number: ")
+        if new.isdigit():
+            items.append(int(new))
+except KeyboardInterrupt:
+    print "\nFinished getting data\n"
+total = 0
+for item in items:
+    total += item
+
+for item in items:
+    div = item / float(total)
+    print str(div) + '\n'
